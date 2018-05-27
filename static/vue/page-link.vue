8263790f2147210d74369b8b1488a1807fa7b8ac
ShuzoN <d.namu.r21@gmail.com>
2018-05-27 21:46:09 +0900

    vue file足してみた

---
+++ static/vue/page-link.vue
@@ -0,0 +1,21 @@
+<template>
+  <table>
+    <tr>
+      <td>
+        <a v-bind:href="link.official">
+          <div>Official Page</div>
+        </a>
+      </td>
+      <td>
+        <a v-bind:href="link.amazon">
+          <img alt="amazon" src="/img/logo/amazon_logo.png" height="30px" />
+        </a>
+      </td>
+      <td>
+        <a v-bind:href="link.eIyahon">
+          <img alt="e☆イヤホン" src="/img/logo/e_iyahon.png" height="30px" />
+        </a>
+      </td>
+    </tr>
+  </table>
+</template>
