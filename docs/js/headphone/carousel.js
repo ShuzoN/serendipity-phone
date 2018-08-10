59e5f08ae5c658366bb6b9b3570b388e97fc3184
ShuzoN <d.namu.r21@gmail.com>
2018-08-11 00:23:13 +0900

    pub

--- docs/js/headphone/carousel.js
+++ docs/js/headphone/carousel.js
@@ -2,9 +2,9 @@ Vue.component('carousel', {
   props: ['items'],
   delimiters: ['${', '}'],
   template: `
-  <v-app id="inspire" style="height:700px;">
-    <v-carousel light hide-delimiters style="height:700px;">
-      <v-carousel-item class="hoge"
+  <v-app>
+    <v-carousel light hide-delimiters id="phone-img-carousel">
+      <v-carousel-item
         v-for="(item,i) in items"
         :key="i"
         :src="item.src"
