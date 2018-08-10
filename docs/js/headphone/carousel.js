f1dc4359934097c1c44807845cba338ce962b1ff
ShuzoN <d.namu.r21@gmail.com>
2018-08-10 22:59:48 +0900

    img-responsiveは影響範囲が広すぎる

--- docs/js/headphone/carousel.js
+++ docs/js/headphone/carousel.js
@@ -2,9 +2,9 @@ Vue.component('carousel', {
   props: ['items'],
   delimiters: ['${', '}'],
   template: `
-  <v-app id="inspire" style="height:400px;">
-    <v-carousel light hide-delimiters style="height:400px;">
-      <v-carousel-item
+  <v-app id="inspire" style="height:700px;">
+    <v-carousel light hide-delimiters style="height:700px;">
+      <v-carousel-item class="hoge"
         v-for="(item,i) in items"
         :key="i"
         :src="item.src"
