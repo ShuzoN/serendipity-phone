e58765c1e6c8db3d7ea52702c6315607fa6e3962
ShuzoN <d.namu.r21@gmail.com>
2018-08-12 18:57:37 +0900

    値段の削除 & google analyticsの追加

--- docs/js/headphone/carousel.js
+++ docs/js/headphone/carousel.js
@@ -2,13 +2,13 @@ Vue.component('carousel', {
   props: ['items'],
   delimiters: ['${', '}'],
   template: `
-  <v-app>
-    <v-carousel light hide-delimiters id="phone-img-carousel">
+  <v-app id="phone-img-carousel">
+    <v-carousel light hide-delimiters>
       <v-carousel-item
         v-for="(item,i) in items"
         :key="i"
         :src="item.src"
-      ></v-carousel-item>
+      id="hoge"></v-carousel-item>
     </v-carousel>
   </v-app>
   `
